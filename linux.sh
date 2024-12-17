e62dd1f61c9a43dc8a305ef823a5cf26e5f6042d
Igor Karatayev <igor.cwer@gmail.com>
2024-12-17 15:04:32 +0300

    linux go prebuild script

---
+++ linux.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+cp ./build/raylib/external/glfw/src/xdg-shell-client-protocol.h ./src/external/glfw/src/
+cp ./build/raylib/external/glfw/src/xdg-shell-client-protocol-code.h ./src/external/glfw/src/
+cp ./build/raylib/external/glfw/src/xdg-decoration-unstable-v1-client-protocol.h ./src/external/glfw/src/
+cp ./build/raylib/external/glfw/src/xdg-decoration-unstable-v1-client-protocol-code.h ./src/external/glfw/src/
+cp ./build/raylib/external/glfw/src/viewporter-client-protocol.h ./src/external/glfw/src/
+cp ./build/raylib/external/glfw/src/viewporter-client-protocol-code.h ./src/external/glfw/src/
+cp ./build/raylib/external/glfw/src/relative-pointer-unstable-v1-client-protocol.h ./src/external/glfw/src/
+cp ./build/raylib/external/glfw/src/relative-pointer-unstable-v1-client-protocol-code.h ./src/external/glfw/src/
+cp ./build/raylib/external/glfw/src/pointer-constraints-unstable-v1-client-protocol.h ./src/external/glfw/src/
+cp ./build/raylib/external/glfw/src/pointer-constraints-unstable-v1-client-protocol-code.h ./src/external/glfw/src/
+cp ./build/raylib/external/glfw/src/fractional-scale-v1-client-protocol.h ./src/external/glfw/src/
+cp ./build/raylib/external/glfw/src/fractional-scale-v1-client-protocol-code.h ./src/external/glfw/src/
+cp ./build/raylib/external/glfw/src/xdg-activation-v1-client-protocol.h ./src/external/glfw/src/
+cp ./build/raylib/external/glfw/src/xdg-activation-v1-client-protocol-code.h ./src/external/glfw/src/
+cp ./build/raylib/external/glfw/src/idle-inhibit-unstable-v1-client-protocol.h ./src/external/glfw/src/
+cp ./build/raylib/external/glfw/src/idle-inhibit-unstable-v1-client-protocol-code.h ./src/external/glfw/src/
+cp ./build/raylib/external/glfw/src/wayland-client-protocol-code.h ./src/external/glfw/src/
